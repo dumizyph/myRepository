6b9fd3c10764c051e37a5c1b243ea9ef95f838a6
itsabdulrafey <ahmedrafey6@gmail.com>
2024-09-16 15:10:31 +0500

    test push

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -67,7 +67,8 @@ const ServiceCard = ({ index, title, icon, description, learnMoreLink }) => {
               position: "relative", // Required for absolute positioning of the line
             }}
           >
-            
+                        {/* The animated line under the title */}
+
             {title}
             {/* The animated line under the title */}
             <Box
